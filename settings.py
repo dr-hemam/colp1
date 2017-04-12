c580e9fcf33325d91b8ed556851c4dd2ae44707b
Hassan Emam <hassan.emam@hotmail.com>
2017-04-12 11:26:09 +0000

    Duplication error catching

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ import os
 
 SECRET_KEY= 'never-guess'
 DEBUG = True
-DB_USER_NAME ='root'
-DB_PASSWORD ='hassan'
+DB_USER_NAME ='hassanemam'
+DB_PASSWORD =''
 DB_NAME = 'colp'
 DB_HOST = os.getenv('IP', '0.0.0.0')
 DB_URI ="mysql+pymysql://%s:%s@%s/%s" %(DB_USER_NAME, DB_PASSWORD, DB_HOST, DB_NAME)
