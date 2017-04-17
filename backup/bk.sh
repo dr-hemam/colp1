c67a3031c26630a70ba481c070db8b1184afab8e
Hassan Emam <hassan.emam@hotmail.com>
2017-04-17 18:46:41 +0000

    Backup

---
+++ backup/bk.sh
@@ -0,0 +1,6 @@
+database=`mysql --user=hassanemam --password="" -e "SHOW DATABASES"`
+for db in $database; do
+    echo "$db"
+    mysqldump --user="hassanemam" --password="" --databases "$db" > ./"$db".sql
+    gzip "$db"
+done
