1b08799ba9dc745aa8df7dd0d50c499486da6dd1
Hassan Emam <hassan.emam@hotmail.com>
2017-05-08 17:54:59 +0000

    Fixing WWP view

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,15 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from colp import app
-import settings
+SECRET_KEY= 'never-guess'
+DEBUG = True
+DB_USER_NAME ='hassanemam'
+DB_PASSWORD =''
+DB_NAME = 'colp'
+DB_HOST = os.getenv('IP', '0.0.0.0')
+DB_URI ="mysql+pymysql://%s:%s@%s/%s" %(DB_USER_NAME, DB_PASSWORD, DB_HOST, DB_NAME)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 if __name__ == "__main__":
 	app.run()
